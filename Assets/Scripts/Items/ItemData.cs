6f1b3b16d155b3854edc13ffee894427c614f7c9
RandomZiom <dkonik10@gmail.com>
2023-04-06 13:40:48 +0200

    Item data class

---
+++ Assets/Scripts/Items/ItemData.cs
@@ -0,0 +1,30 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Items
+{
+    public class ItemData: SerializedScriptableObject
+    {
+        [SerializeField]
+        private string _displayName;
+        
+        [SerializeField]
+        private Sprite _icon;
+        
+        [SerializeField]
+        private string _description;
+        
+        [SerializeField]
+        private int _maxStack;
+
+        public string DisplayName => _displayName;
+
+        public Sprite Icon => _icon;
+
+        public string Description => _description;
+
+        public int MaxStack => _maxStack;
+        
+        public bool IsStackable => _maxStack > 1;
+    }
+}
