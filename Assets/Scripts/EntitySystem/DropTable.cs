a48e8c8137a7d8289b6cf179dddacf0ffbb8efcb
RandomZiom <dkonik10@gmail.com>
2023-04-14 12:30:50 +0200

    sample resource, inventory fix, entity drop table

---
+++ Assets/Scripts/EntitySystem/DropTable.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Items;
+using Sirenix.Serialization;
+
+namespace EntitySystem
+{
+    [Serializable]
+    public class DropTable
+    {
+        [OdinSerialize]
+        private List<ItemDrop> _drops = new();
+        
+        public List<Item> GetDrops()
+        {
+            return (from drop in _drops 
+                where drop.Chance >= UnityEngine.Random.value 
+                select drop.Item)
+                .ToList();
+        }
+    }
+}
