a24cc2e9f28b90b2b933f164f23dfe13b33236c8
RandomZiom <dkonik10@gmail.com>
2023-05-01 15:47:37 +0200

    Added damage indicator

---
+++ Assets/Scripts/EntitySystem/DamageIndicator.cs
@@ -0,0 +1,34 @@
+﻿using Cysharp.Threading.Tasks;
+using DG.Tweening;
+using TMPro;
+using UnityEngine;
+
+namespace EntitySystem
+{
+    [RequireComponent(typeof(TMP_Text))]
+    public class DamageIndicator: MonoBehaviour
+    {
+        private TMP_Text _text;
+        
+        private async void Awake()
+        {
+            _text = GetComponent<TMP_Text>();
+            
+            var randomDirection = Random.insideUnitCircle.normalized;
+            transform.DOMove(transform.position + new Vector3(randomDirection.x * .25f, randomDirection.y * .25f, 0), .2f);
+            
+            await UniTask.Delay(1000);
+            
+            transform.DOScale(Vector3.zero, .2f).OnComplete(
+                () => Destroy(gameObject));
+        }
+        
+        public void SetDamage(float damage)
+        {
+            var rounded = Mathf.RoundToInt(damage);
+            
+            _text.SetText(rounded.ToString());
+            _text.color = damage > 0 ? Color.white : Color.red;
+        }
+    }
+}
---
+++ Assets/Scripts/EntitySystem/EntityDamageIndicatorSpawner.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using DG.Tweening;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace EntitySystem
+{
+    [RequireComponent(typeof(Entity))]
+    public class EntityDamageIndicatorSpawner: MonoBehaviour
+    {
+        [SerializeField] 
+        private DamageIndicator _damageIndicator;
+        
+        private Entity _entity;
+        
+        private void Awake()
+        {
+            _entity = GetComponent<Entity>();
+            _entity.OnDamageTaken += SpawnDamageIndicator;
+        }
+
+        private void OnDestroy()
+        {
+            _entity.OnDamageTaken -= SpawnDamageIndicator;
+        }
+
+        private void SpawnDamageIndicator(float damage)
+        {
+            var indicator = Instantiate(_damageIndicator, transform.position, Quaternion.identity);
+            indicator.SetDamage(damage);
+        }
+    }
+}
