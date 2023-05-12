27ca2d877c99e1497cf5883b71ad7fbbd2c124be
Dominik <38404029+RandomowyRandom@users.noreply.github.com>
2023-05-12 13:47:53 +0200

    Updated enemy behaviour (#82)
    Co-authored-by: RandomZiom <dkonik10@gmail.com>

---
+++ Assets/Scripts/EntitySystem/EntityTask/PlayAnimationEntityTask.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Cysharp.Threading.Tasks;
+using EntitySystem.Abstraction;
+using UnityEngine;
+
+namespace EntitySystem.EntityTask
+{
+    [Serializable]
+    public class PlayAnimationEntityTask: IEntityTask
+    {
+        [SerializeField] 
+        private Animator _animator;
+        
+        [SerializeField]
+        private string _animationName;
+        
+        [SerializeField]
+        private bool _waitUntilAnimationEnds;
+        public async UniTask<EnemyTaskResult> Execute(EntityBehaviour entity)
+        {
+            _animator.Play(_animationName);
+
+            if(_waitUntilAnimationEnds)
+                await UniTask.Delay(TimeSpan.FromSeconds(_animator.GetCurrentAnimatorStateInfo(0).length));
+
+            return EnemyTaskResult.Completed;
+        }
+    }
+}
---
+++ Assets/Scripts/EntitySystem/EntityTask/ShootProjectilesAroundEntityTask.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Cysharp.Threading.Tasks;
+using EntitySystem.Abstraction;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace EntitySystem.EntityTask
+{
+    [Serializable]
+    public class ShootProjectilesAroundEntityTask: IEntityTask
+    {
+        [SerializeField]
+        private Rigidbody2D _projectilePrefab;
+        
+        [SerializeField]
+        private float _force;
+        
+        [SerializeField]
+        private float _amount;
+        
+        public UniTask<EnemyTaskResult> Execute(EntityBehaviour entity)
+        {
+            if (entity == null)
+                return UniTask.FromResult(EnemyTaskResult.Break);
+            
+            var angleStep = 360f / _amount;
+            var angle = 0f;
+            
+            for (var i = 0; i < _amount; i++)
+            {
+                var projectile = Object.Instantiate(_projectilePrefab, entity.transform.position, Quaternion.identity);
+                projectile.AddForce(new Vector2(Mathf.Cos(angle * Mathf.Deg2Rad), Mathf.Sin(angle * Mathf.Deg2Rad)).normalized * _force, ForceMode2D.Impulse);
+                angle += angleStep;
+            }
+            
+            return UniTask.FromResult(EnemyTaskResult.Completed);
+        }
+    }
+}
