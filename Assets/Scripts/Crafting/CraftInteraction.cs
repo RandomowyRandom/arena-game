51fc359abbec9ae54a5d438e3532bb9de58c99d6
Dominik <38404029+RandomowyRandom@users.noreply.github.com>
2023-05-13 21:01:14 +0200

    Crafting rework (#85)
    * removed unused RGP assets
    
    * npc spawning tables
    
    * moved crafting to npc
    
    ---------
    
    Co-authored-by: RandomZiom <dkonik10@gmail.com>

---
+++ Assets/Scripts/Crafting/CraftInteraction.cs
@@ -0,0 +1,52 @@
+﻿using Crafting.Abstraction;
+using InteractionSystem;
+using InteractionSystem.Abstraction;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Crafting
+{
+    public class CraftInteraction: SerializedMonoBehaviour, IInteractable
+    {
+        [SerializeField]
+        private SpriteRenderer _itemRenderer;
+        
+        [SerializeField]
+        private InteractionTextHandler _interactionTextHandler;
+        
+        public ICraftingHandler CraftingHandler { get; set; }
+        
+        private OutlineInteractionEffect _outlineInteractionEffect;
+        public GameObject GameObject => gameObject;
+
+        private CraftingRecipe _recipe;
+        
+        private void Awake()
+        {
+            _outlineInteractionEffect = GetComponent<OutlineInteractionEffect>();
+        }
+
+        public void SetRecipe(CraftingRecipe recipe)
+        {
+            _recipe = recipe;
+            _itemRenderer.sprite = recipe.Result.ItemData.Icon;
+        }
+        
+        public void Interact()
+        {
+            CraftingHandler.TryCraft(_recipe);
+        }
+
+        public void OnHandlerEnter(IInteractionHandler handler)
+        {
+            _outlineInteractionEffect?.Show();
+            _interactionTextHandler?.Show();
+        }
+
+        public void OnHandlerExit(IInteractionHandler handler)
+        {
+            _outlineInteractionEffect?.Hide();
+            _interactionTextHandler?.Hide();
+        }
+    }
+}
---
+++ Assets/Scripts/Crafting/CraftingNpc.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Common.Extensions;
+using Crafting.Abstraction;
+using Inventory.Interfaces;
+using Player.Interfaces;
+using Sirenix.OdinInspector;
+using Sirenix.Serialization;
+using UnityEngine;
+using UnityEngine.Serialization;
+using WaveSystem;
+
+namespace Crafting
+{
+    public class CraftingNpc: SerializedMonoBehaviour
+    {
+        [OdinSerialize]
+        private ICraftingHandler _craftingHandler;
+        
+        [SerializeField]
+        private CraftInteraction _craftInteractionPrefab;
+        
+        [OdinSerialize]
+        private List<CraftingStationData> _craftingStationDatas;
+
+        private IPlayerLevel _playerLevel;
+
+        private IWaveManager _waveManager;
+        
+        private void Start()
+        {
+            _playerLevel = ServiceLocator.ServiceLocator.Instance.Get<IPlayerLevel>();
+            _waveManager = ServiceLocator.ServiceLocator.Instance.Get<IWaveManager>();
+            InstantiateCraftInteractions();
+            
+            _waveManager.OnWaveStart += HideNpc;
+            _waveManager.OnWaveEnd += ShowNpc;
+            
+            HideNpc(null);
+        }
+
+        private void OnDestroy()
+        {
+            _waveManager.OnWaveStart -= HideNpc;
+            _waveManager.OnWaveEnd -= ShowNpc;
+        }
+
+        private void ShowNpc(Wave obj)
+        {
+            gameObject.SetActive(true);
+            InstantiateCraftInteractions();
+        }
+
+        private void HideNpc(Wave obj)
+        {
+            gameObject.SetActive(false);
+        }
+
+        private void InstantiateCraftInteractions()
+        {
+            DestroyCraftInteractions();
+
+            var y = -.7f;
+            var x = -2f;
+            
+            foreach (var data in _craftingStationDatas)
+            {
+                var recipes = _craftingHandler.CraftingRecipeDatabase
+                    .GetRecipesByTypeAndLevel(data.ItemType, _playerLevel.CurrentLevel - 2, _playerLevel.CurrentLevel);
+                
+                recipes.Shuffle();
+                
+                var recipesToDisplay = recipes.Take(data.Amount).ToList();
+                
+                foreach (var recipe in recipesToDisplay)
+                {
+                    var interaction = Instantiate(
+                        _craftInteractionPrefab, transform.position + new Vector3(x, y), Quaternion.identity, 
+                        transform);
+                
+                    interaction.CraftingHandler = _craftingHandler;
+                    interaction.SetRecipe(recipe);
+
+                    x++;
+                }
+                
+                x++;
+            }
+        } 
+        
+        private void DestroyCraftInteractions()
+        {
+            foreach (Transform child in transform)
+            {
+                if(child.GetComponent<CraftInteraction>() == null)
+                    continue;
+                
+                Destroy(child.gameObject);
+            }
+        }
+    }
+}
