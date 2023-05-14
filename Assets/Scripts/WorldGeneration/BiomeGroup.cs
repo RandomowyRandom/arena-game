eca4017bc3af2cdd15ba3613d945397b78a4ee98
RandomZiom <dkonik10@gmail.com>
2023-05-14 15:59:03 +0200

    working sub biomes generator

---
+++ Assets/Scripts/WorldGeneration/BiomeGroup.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using Sirenix.Serialization;
+
+namespace WorldGeneration
+{
+    [Serializable]
+    public class BiomeGroup
+    {
+        [OdinSerialize]
+        private List<BiomeTerrainGenerator> _biomeTerrainGenerators;
+        
+        public List<BiomeTerrainGenerator> BiomeTerrainGenerators => _biomeTerrainGenerators;
+    }
+}
