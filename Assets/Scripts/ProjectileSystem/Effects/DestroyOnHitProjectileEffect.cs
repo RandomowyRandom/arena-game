18c085cd803b0ee645799f6320db66207e435a45
RandomZiom <dkonik10@gmail.com>
2023-05-15 11:29:30 +0200

    balance changes

---
+++ Assets/Scripts/ProjectileSystem/Effects/DestroyOnHitProjectileEffect.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Cysharp.Threading.Tasks;
+using EntitySystem;
+using ProjectileSystem.Abstraction;
+
+namespace ProjectileSystem.Effects
+{
+    [Serializable]
+    public class DestroyOnHitProjectileEffect: IProjectileEffect
+    {
+        public UniTask ApplyHitEffect(Projectile projectile, Entity entity)
+        {
+            UnityEngine.Object.Destroy(projectile.gameObject);
+            
+            return UniTask.CompletedTask;
+        }
+    }
+}
