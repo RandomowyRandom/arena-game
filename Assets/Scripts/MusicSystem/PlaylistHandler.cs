92f6051b5c5ec615067a16451a224b9dcbcdebc3
RandomZiom <dkonik10@gmail.com>
2023-05-18 20:03:30 +0200

    music looping

--- Assets/Scripts/MusicSystem/PlaylistHandler.cs
+++ Assets/Scripts/MusicSystem/PlaylistHandler.cs
@@ -60,12 +60,13 @@ namespace MusicSystem
             await PlayTrackUntilFinished(transitionTrack, 0f, 1f);
             
             PlayTrack(track, 1f, 1f);
+            _audioSource.loop = true;
         }
 
         private void PlayCombatTrack(Wave wave)
         {
             var track = _playlist.CombatClip;
-            PlayTrack(track, 1f, 1f);
+            PlayTrack(track, .4f, 1f);
         }
 
         private void PlayTrack(AudioClip clip, float fromTime = 0f, float toTime = 0f)
@@ -80,8 +81,10 @@ namespace MusicSystem
 
         private async UniTask PlayTrackUntilFinished(AudioClip clip, float fromTime = 0f, float toTime = 0f)
         {
+            _audioSource.loop = false;
             PlayTrack(clip, fromTime, toTime);
             await UniTask.Delay(TimeSpan.FromSeconds(clip.length));
+            _audioSource.clip = null;
         }
         
     }
